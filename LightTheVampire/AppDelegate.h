54961139678005b0b82ebad64812ca3e8f7a8663
Denis Nosulenko <synedoff@gmail.com>
2012-06-11 15:01:38 +0300

    Initial Commit

---
+++ LightTheVampire/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  LightTheVampire
+//
+//  Created by Denis Nosulenko on 6/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
